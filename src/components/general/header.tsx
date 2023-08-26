01f89bcb30394fb3e771429fedb32ecef1f18f20
TonyaKapustina <tonuakapystina@gmail.com>
2023-08-26 17:39:23 +0200

    Update toggle

--- src/components/general/header.tsx
+++ src/components/general/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     const [value, setValue] = useState(false);
     return (
         <div className="bg-blue-700">
-            <div className="container mx-auto px-2 py-3 sm:px-6 lg:px-8 text-white text-xl">
+            <div className="container mx-auto px-2 py-3 sm:px-6 lg:px-8 text-white text-xl flex flex-row justify-between">
                 Note manager
                 <ToggleSwitch
                     isOn={value}
--- src/components/general/toggleSwitch.tsx
+++ src/components/general/toggleSwitch.tsx
@@ -5,23 +5,25 @@ type ToggleSwitchPropsType = {
     handleToggle: (value: boolean) => void;
 };
 
-export const ToggleSwitch: FC<ToggleSwitchPropsType> = ({ isOn, handleToggle }) => {
+export const ToggleSwitch: FC<ToggleSwitchPropsType> = ({isOn, handleToggle}) => {
 
     return (
-        <>
+        <div className='flex flex-row items-center'>
+            <label className='mr-5'>Advanced Search Mode</label>
             <input
                 className="react-switch-checkbox"
-                id={`react-switch-new`}
+                id="react-switch-new"
                 type="checkbox"
                 checked={isOn}
                 onChange={handleToggle}
             />
             <label
+                style={{background: isOn && '#06D6A0'}}
                 className="react-switch-label"
-                htmlFor={`react-switch-new`}
+                htmlFor="react-switch-new"
             >
-                <span className={`react-switch-button`} />
+                <span className={`react-switch-button`}/>
             </label>
-        </>
+        </div>
     );
 };
